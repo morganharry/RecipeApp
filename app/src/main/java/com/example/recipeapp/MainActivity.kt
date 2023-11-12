0c8f9191fbfc173e49bcaa4e2c7fcac30c3139b4
Harry Morgan <west_ham@mail.ru>
2023-11-12 12:58:57 +0600

    RA-09_added fragment_list_recipes

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         get() = _binding
             ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
