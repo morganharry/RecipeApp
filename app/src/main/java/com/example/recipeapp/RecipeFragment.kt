eaae5bed2203c0dfa44ac6db09803564454de02b
Harry Morgan <west_ham@mail.ru>
2023-11-22 22:57:30 +0600

    RA-13 fixed

--- app/src/main/java/com/example/recipeapp/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/RecipeFragment.kt
@@ -60,12 +60,10 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
         val methodAdapter = recipeMethod?.let { MethodAdapter(it, this) }
 
         val recyclerIngredientsView: RecyclerView = binding.rvIngredients
-
         getContext()?.let { RecyclerViewItemDecoration(it, R.drawable.shape_divider) }
             ?.let { recyclerIngredientsView.addItemDecoration(it) }
 
         val recyclerMethodView: RecyclerView = binding.rvMethod
-
         getContext()?.let { RecyclerViewItemDecoration(it, R.drawable.shape_divider) }
             ?.let { recyclerMethodView.addItemDecoration(it) }
 
