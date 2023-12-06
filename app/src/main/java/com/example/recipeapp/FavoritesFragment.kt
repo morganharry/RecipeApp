0d171cb1915f2b997947b4c994f96754f4805de1
Harry Morgan <west_ham@mail.ru>
2023-12-06 22:42:31 +0600

    RA-18 initial

--- app/src/main/java/com/example/recipeapp/FavoritesFragment.kt
+++ app/src/main/java/com/example/recipeapp/FavoritesFragment.kt
@@ -1,13 +1,10 @@
 package com.example.recipeapp
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.recipeapp.data.APP_RECIPES
-import com.example.recipeapp.data.APP_RECIPES_SET_STRING
 import com.example.recipeapp.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
@@ -29,12 +26,8 @@ class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvTest.text = getFavorites()?.first() ?: null
+
 
     }
-    private fun getFavorites(): MutableSet<String>? {
-        val sharedPrefs = context?.getSharedPreferences(APP_RECIPES, Context.MODE_PRIVATE)
-        val setOfId = sharedPrefs?.getStringSet(APP_RECIPES_SET_STRING, null)
-        return setOfId
-    }
+
 }
