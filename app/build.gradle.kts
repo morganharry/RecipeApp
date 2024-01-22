c591e29a264c36cd63f86c9135c75e34045353e4
HM <west_ham@mail.ru>
2024-01-22 16:54:42 +0600

    RA-35_added_implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
 }
