98c75fe0ea114fb671b82bcc62e923a60f1fe70b
Harry Morgan <west_ham@mail.ru>
2024-02-03 15:18:34 +0600

    RA-37_added_ViewModelScope

--- app/src/main/java/com/example/recipeapp/data/RecipesRepository.kt
+++ app/src/main/java/com/example/recipeapp/data/RecipesRepository.kt
@@ -4,6 +4,8 @@ import com.example.recipeapp.model.Category
 import com.example.recipeapp.model.Recipe
 import com.example.recipeapp.model.URL_API
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -33,62 +35,74 @@ class RecipesRepository {
         return retrofit.create(RecipeApiService::class.java)
     }
 
-    fun getCategories(): List<Category>? {
+    suspend fun getCategories(): List<Category>? {
         return try {
             val service = createService()
 
-            val categoriesCall: Call<List<Category>> = service.getCategories()
-            val categoriesResponse: Response<List<Category>> = categoriesCall.execute()
-            categoriesResponse.body()
+            withContext(Dispatchers.IO) {
+                val categoriesCall: Call<List<Category>> = service.getCategories()
+                val categoriesResponse: Response<List<Category>> = categoriesCall.execute()
+                categoriesResponse.body()
+            }
         } catch (e: Exception) {
             null
         }
     }
 
 
-    fun getCategory(id: Int): Category? {
+    suspend fun getCategory(id: Int): Category? {
         return try {
             val service = createService()
 
-            val categoryCall: Call<Category> = service.getCategory(id)
-            val categoryResponse: Response<Category> = categoryCall.execute()
-            categoryResponse.body()
+            withContext(Dispatchers.IO) {
+                val categoryCall: Call<Category> = service.getCategory(id)
+                val categoryResponse: Response<Category> = categoryCall.execute()
+                categoryResponse.body()
+            }
         } catch (e: Exception) {
             null
         }
     }
 
-    fun getRecipesByCategory(id: Int): List<Recipe>? {
+    suspend fun getRecipesByCategory(id: Int): List<Recipe>? {
         return try {
             val service = createService()
 
-            val recipesCall: Call<List<Recipe>> = service.getRecipesByCategory(id)
-            val recipesResponse: Response<List<Recipe>> = recipesCall.execute()
-            recipesResponse.body()
+            withContext(Dispatchers.IO) {
+                val recipesCall: Call<List<Recipe>> = service.getRecipesByCategory(id)
+                val recipesResponse: Response<List<Recipe>> = recipesCall.execute()
+                recipesResponse.body()
+            }
         } catch (e: Exception) {
             null
         }
     }
 
-    fun getRecipe(id: Int): Recipe? {
+    suspend fun getRecipe(id: Int): Recipe? {
         return try {
             val service = createService()
 
-            val recipeCall: Call<Recipe> = service.getRecipe(id)
-            val recipeResponse: Response<Recipe> = recipeCall.execute()
-            recipeResponse.body()
+            withContext(Dispatchers.IO) {
+                val recipeCall: Call<Recipe> = service.getRecipe(id)
+                val recipeResponse: Response<Recipe> = recipeCall.execute()
+                recipeResponse.body()
+            }
         } catch (e: Exception) {
             null
         }
     }
 
-    fun getRecipes(ids: String): List<Recipe>? {
+    suspend fun getRecipes(ids: String): List<Recipe>? {
+
+
         return try {
             val service = createService()
 
-            val recipesCall: Call<List<Recipe>> = service.getRecipes(ids)
-            val recipesResponse: Response<List<Recipe>> = recipesCall.execute()
-            recipesResponse.body()
+            withContext(Dispatchers.IO) {
+                val recipesCall: Call<List<Recipe>> = service.getRecipes(ids)
+                val recipesResponse: Response<List<Recipe>> = recipesCall.execute()
+                recipesResponse.body()
+            }
         } catch (e: Exception) {
             null
         }
